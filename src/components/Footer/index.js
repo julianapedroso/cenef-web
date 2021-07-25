6d94e56ca57351c34dff5603306702b53d71835a
AngeloVSO <angelo_odwyer@hotmail.com>
2021-07-25 16:19:42 -0300

    Estilização do footer + responsividade

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,24 @@
-import { FooterPage } from "./styles";
+import { FooterPage, SocialMedia } from "./styles";
+import LogoFacebook from "../../assets/facebook.svg";
+import LogoInstagram from "../../assets/instagram.svg";
 
 const Footer = () => {
   return (
     <>
       <FooterPage>
         <div>
-            <p>insta</p>
-            <p>face</p>
+          <div>
+            <span>Mídias</span>
+            <SocialMedia>
+              <a target="_blank" rel="noopener noreferrer" href="https://pt-br.facebook.com/cenef.jlle/">
+                <img src={LogoFacebook} alt="Logo facebook" />
+              </a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/cenefjoinville/">
+                <img src={LogoInstagram} alt="Logo instagram" />
+              </a>
+            </SocialMedia>
+          </div>
+          <p>cenef.jlle@gmail.com</p>
         </div>
       </FooterPage>
     </>
