c0c70bbc39547ee01648ddfb804eedb45916f271
julianapedroso <juliana.cristina@hotmail.com.br>
2021-07-26 02:48:20 -0300

    creation of contacts page and responsiveness

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,34 @@
+import React from "react";
+import ContactUs from "../../assets/contact.png";
+import { Image, Main, Section } from "./styles";
+
 const Contact = () => {
-    return (
-      <div>
-          Contato
-      </div>
-    );
-  };
-  
-  export default Contact;
-  
+  return (
+    <Main>
+      <Section>
+        <div>
+          <h1>Contato</h1>
+          <div>
+            <p>
+              Endereço:
+              <br />
+              Rua Urussanga, 444 - Bucarein
+              <br />
+              CEP 89202-400
+              <br />
+              Joinville/SC
+            </p>
+            <p>
+              Telefone: (47) 3422-7033
+              <br />
+              E-mail: cenef.jlle@gmail.com
+            </p>
+          </div>
+        </div>
+        <Image src={ContactUs} alt="Mão sobre o notebook segurando iPhone" />
+      </Section>
+    </Main>
+  );
+};
+
+export default Contact;
